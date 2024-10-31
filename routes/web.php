41d3195aa653593f927c263a7bf979d71e0aea90
leonardogeja <Leonardo.geja@unoesc.edu.br>
2024-10-31 13:18:32 -0300

    feat: adicionado pagina de plano

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PlanoDeEnsinoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/plano-de-ensino/cadastrar', [PlanoDeEnsinoController::class, 'index'])->name('plano.index');
